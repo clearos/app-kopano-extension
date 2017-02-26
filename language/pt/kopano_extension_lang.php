1ff98e9dd871f74fd83f08dffa99fbd742e43054
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:10:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'União Extensão';
+$lang['kopano_extension_app_description'] = 'A inscrição União Extensão'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'União Contas Extensão';
+$lang['kopano_extension_administrator_privileges'] = 'privilégios de administrador';
+$lang['kopano_extension_hard_quota'] = 'Quota disco';
+$lang['kopano_extension_account'] = 'Conta';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'bandeira da conta é inválido.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'flag Administrador é inválido.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'cota máxima é inválido.';
+$lang['kopano_extension_quota_override'] = 'Substituir quota';
+$lang['kopano_extension_soft_quota'] = 'Citar software';
+$lang['kopano_extension_warning_quota'] = 'Quota de advertência';
