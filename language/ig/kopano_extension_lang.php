eb4c75672883213a26d85ec880c7d5092cc1ec42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:06:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension nkọwa'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator Ùgwù';
+$lang['kopano_extension_hard_quota'] = 'ike oke';
+$lang['kopano_extension_account'] = 'akaụntụ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Account ọkọlọtọ bụ ghara ịdị irè.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator ọkọlọtọ bụ ghara ịdị irè.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Ike oke bụ ghara ịdị irè.';
+$lang['kopano_extension_quota_override'] = 'oke akpagbu';
+$lang['kopano_extension_soft_quota'] = 'software Quote';
+$lang['kopano_extension_warning_quota'] = 'ịdọ aka ná ntị oke';
