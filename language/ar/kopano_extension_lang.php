79372e7d540958073f08f6026f3b71b5f4a938b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:02:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'الاتحاد التمديد';
+$lang['kopano_extension_app_description'] = 'وصف الاتحاد التمديد'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'الحسابات تمديد الاتحاد';
+$lang['kopano_extension_administrator_privileges'] = 'امتيازات المسؤول';
+$lang['kopano_extension_hard_quota'] = 'حصة الصعبة';
+$lang['kopano_extension_account'] = 'الحساب';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'علم الحساب غير صالح.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'علم المسؤول غير صالح.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'الحصة النسبية الثابت غير صالحة.';
+$lang['kopano_extension_quota_override'] = 'الحصة تجاوز';
+$lang['kopano_extension_soft_quota'] = 'البرنامج اقتباس';
+$lang['kopano_extension_warning_quota'] = 'حصة تحذير';
