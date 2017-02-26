48872d503e6ac4af78c6eb13a282f26c689d1841
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:13:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Liên minh mở rộng';
+$lang['kopano_extension_app_description'] = 'Mô tả công đoàn mở rộng'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Liên minh Tài khoản mở rộng';
+$lang['kopano_extension_administrator_privileges'] = 'Ưu đãi quản trị viên';
+$lang['kopano_extension_hard_quota'] = 'Hạn ngạch cứng';
+$lang['kopano_extension_account'] = 'Tài khoản';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'cờ tài khoản không hợp lệ.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Quản trị viên cờ là không hợp lệ.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'hạn ngạch cứng là không hợp lệ.';
+$lang['kopano_extension_quota_override'] = 'Hạn ngạch Override';
+$lang['kopano_extension_soft_quota'] = 'phần mềm Trích dẫn';
+$lang['kopano_extension_warning_quota'] = 'Cảnh báo hạn ngạch';
