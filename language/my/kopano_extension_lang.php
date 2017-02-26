828fcfae269026ca026155c425ade33cecd19282
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:03:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ပြည်ထောင်စုတိုးချဲ့မှု';
+$lang['kopano_extension_app_description'] = 'ပြည်ထောင်စုတိုးချဲ့မှုဖော်ပြချက်'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ပြည်ထောင်စု Accounts ကိုတိုးချဲ့';
+$lang['kopano_extension_administrator_privileges'] = 'အုပ်ချုပ်ရေးမှူးအခွင့်ထူး';
+$lang['kopano_extension_hard_quota'] = 'hard ခွဲတမ်း';
+$lang['kopano_extension_account'] = 'အကောင့်';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'အကောင့်အလံမမှန်ကန်ဘူး။';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'အုပ်ချုပ်ရေးမှူးအလံမမှန်ကန်ဘူး။';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'hard ခွဲတမ်းမမှန်ကန်ဘူး။';
+$lang['kopano_extension_quota_override'] = 'ခွဲတမ်းရပ်တန့်ရန်';
+$lang['kopano_extension_soft_quota'] = 'software ကို Quote';
+$lang['kopano_extension_warning_quota'] = 'သတိပေးခွဲတမ်း';
