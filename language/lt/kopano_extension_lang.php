2fb88bcc6f67e965f7ba9e55641b48c356d4804b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Sąjungos išplėtimas';
+$lang['kopano_extension_app_description'] = 'Sąjungos išplėtimas aprašymas'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Sąjungos sąskaitų pratęsimas';
+$lang['kopano_extension_administrator_privileges'] = 'administratoriaus privilegijomis';
+$lang['kopano_extension_hard_quota'] = 'sunku kvota';
+$lang['kopano_extension_account'] = 'sąskaita';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Sąskaita vėliava yra neteisinga.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administratorius vėliava yra neteisinga.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Sunku kvota yra neteisinga.';
+$lang['kopano_extension_quota_override'] = 'kvota Valdymas';
+$lang['kopano_extension_soft_quota'] = 'programinė įranga Citata';
+$lang['kopano_extension_warning_quota'] = 'Įspėjimas kvota';
