30bb3b87233e7a1172281bd16735f6913e0e6041
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:13:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'פֿאַרבאַנד עקסטענסיאָן';
+$lang['kopano_extension_app_description'] = 'פֿאַרבאַנד געשפּרייט באַשרייַבונג'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'פֿאַרבאַנד אַקקאָונץ עקסטענסיאָן';
+$lang['kopano_extension_administrator_privileges'] = 'אַדמיניסטראַטאָר פּריווילעגעס';
+$lang['kopano_extension_hard_quota'] = 'שווער קוואָטאַ';
+$lang['kopano_extension_account'] = 'באַריכטן';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'אַקאַונט פאָן איז פאַרקריפּלט.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'אַדמיניסטראַטאָר פאָן איז פאַרקריפּלט.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'שווער קוואָטע איז פאַרקריפּלט.';
+$lang['kopano_extension_quota_override'] = 'קוואָטע אָוועררידע';
+$lang['kopano_extension_soft_quota'] = 'ווייכווארג ציטירן';
+$lang['kopano_extension_warning_quota'] = 'ווארענונג קוואָטאַ';
