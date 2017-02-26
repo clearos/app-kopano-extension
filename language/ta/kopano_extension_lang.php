93600a95c140da9620a651498b96b8fd808d95d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:12:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ஒன்றிய நீட்டிப்பு';
+$lang['kopano_extension_app_description'] = 'ஒன்றிய நீட்டிப்பு விளக்கம்'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ஒன்றிய கணக்குகள் நீட்டிப்பு';
+$lang['kopano_extension_administrator_privileges'] = 'நிர்வாகி உரிமைகளுடன்';
+$lang['kopano_extension_hard_quota'] = 'ஹார்ட் ஒதுக்கீடு';
+$lang['kopano_extension_account'] = 'கணக்கு';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'கணக்கு கொடி தவறானது.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'நிர்வாகி கொடி தவறானது.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ஹார்ட் ஒதுக்கீடு தவறானது.';
+$lang['kopano_extension_quota_override'] = 'ஒதுக்கீடு மீறு';
+$lang['kopano_extension_soft_quota'] = 'மென்பொருள் ஆனால்';
+$lang['kopano_extension_warning_quota'] = 'எச்சரிக்கை ஒதுக்கீடு';
