abeb0e019cf957a59dc09a7acea92041677729b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:10:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'اتحادیه فرمت';
+$lang['kopano_extension_app_description'] = 'توضیحات اتحادیه فرمت'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'اتحادیه حساب فرمت';
+$lang['kopano_extension_administrator_privileges'] = 'دسترسی مدیر';
+$lang['kopano_extension_hard_quota'] = 'سهمیه سخت';
+$lang['kopano_extension_account'] = 'حساب';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'پرچم حساب نامعتبر است.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'پرچم مدیر نامعتبر است.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'سهمیه سخت نامعتبر است.';
+$lang['kopano_extension_quota_override'] = 'نادیده گرفتن سهمیه';
+$lang['kopano_extension_soft_quota'] = 'نرم افزار نقل قول';
+$lang['kopano_extension_warning_quota'] = 'سهمیه هشدار';
