0fe7dad7ae0acd8c07e16f376e5743b3e4671335
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:02:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Unie Uitbreiding';
+$lang['kopano_extension_app_description'] = 'Unie Uitbreiding beskrywing'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Unie Rekeninge Uitbreiding';
+$lang['kopano_extension_administrator_privileges'] = 'administrateurs';
+$lang['kopano_extension_hard_quota'] = 'hard Kwota';
+$lang['kopano_extension_account'] = 'rekening';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Rekening vlag is ongeldig.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrateur vlag is ongeldig.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kwota is ongeldig.';
+$lang['kopano_extension_quota_override'] = 'kwota ignoreer';
+$lang['kopano_extension_soft_quota'] = 'sagteware Haal';
+$lang['kopano_extension_warning_quota'] = 'waarskuwing Kwota';
