0706d788b6df8165afeb2f3954105066e0c5eb6f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:11:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union penyuluh';
+$lang['kopano_extension_app_description'] = 'katerangan Union penyuluh'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Akun penyuluh';
+$lang['kopano_extension_administrator_privileges'] = 'statusna administrator';
+$lang['kopano_extension_hard_quota'] = 'hard kuota';
+$lang['kopano_extension_account'] = 'rekening';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Akun bandéra anu sah.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator bandéra anu sah.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kuota anu sah.';
+$lang['kopano_extension_quota_override'] = 'kuota Override';
+$lang['kopano_extension_soft_quota'] = 'software Quote';
+$lang['kopano_extension_warning_quota'] = 'kuota Perhatosan';
