6f4b7b4e7f3a0ac023949a95d7dc4fb7820f8f4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:13:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Estyniad Union';
+$lang['kopano_extension_app_description'] = 'Disgrifiad Estyniad Union'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Cyfrifon Undeb Estyniad';
+$lang['kopano_extension_administrator_privileges'] = 'Breintiau Gweinyddwr';
+$lang['kopano_extension_hard_quota'] = 'Cwota caled';
+$lang['kopano_extension_account'] = 'cyfrif';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'baner cyfrif yn annilys.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'baner Gweinyddwr yn annilys.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'cwota caled yn annilys.';
+$lang['kopano_extension_quota_override'] = 'Diystyru cwota';
+$lang['kopano_extension_soft_quota'] = 'Dyfyniad meddalwedd';
+$lang['kopano_extension_warning_quota'] = 'Cwota rhybudd';
