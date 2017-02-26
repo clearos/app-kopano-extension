42300e005800d4ac5c8e6843ab99732c164ac8c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:14:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'union Itẹsiwaju';
+$lang['kopano_extension_app_description'] = 'Union Itẹsiwaju apejuwe'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union iroyin Itẹsiwaju';
+$lang['kopano_extension_administrator_privileges'] = 'IT anfaani';
+$lang['kopano_extension_hard_quota'] = 'lile meeli';
+$lang['kopano_extension_account'] = 'Account';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Account flag ni invalid.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'IT flag ni invalid.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Lile meeli ti wa ni invalid.';
+$lang['kopano_extension_quota_override'] = 'meeli idojuk';
+$lang['kopano_extension_soft_quota'] = 'software Quote';
+$lang['kopano_extension_warning_quota'] = 'ìkìlọ fun meeli';
