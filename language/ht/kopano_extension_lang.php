05d4ab2bcc949522ddf877d8b9e8fb1f9cb5063a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:05:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Inyon Ekstansyon';
+$lang['kopano_extension_app_description'] = 'Inyon Ekstansyon deskripsyon'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Inyon Kont Ekstansyon';
+$lang['kopano_extension_administrator_privileges'] = 'administratè Privilèj';
+$lang['kopano_extension_hard_quota'] = 'Hard Kota';
+$lang['kopano_extension_account'] = 'Kont';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'drapo Kont se valab.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administratè drapo se valab.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kota se valab.';
+$lang['kopano_extension_quota_override'] = 'kota Depase';
+$lang['kopano_extension_soft_quota'] = 'lojisyèl Quote';
+$lang['kopano_extension_warning_quota'] = 'Avètisman Kota';
