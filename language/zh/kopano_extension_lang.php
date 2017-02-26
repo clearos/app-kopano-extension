a72fce221ddc5c188668a87ea8b9e23ae2db9db3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = '联盟推广';
+$lang['kopano_extension_app_description'] = '联盟扩展描述'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = '欧盟账户扩展';
+$lang['kopano_extension_administrator_privileges'] = '管理员权限';
+$lang['kopano_extension_hard_quota'] = '硬配额';
+$lang['kopano_extension_account'] = '帐户';
+
+$lang['kopano_extension_account_flag_is_invalid'] = '帐户标志是无效的。';
+$lang['kopano_extension_administrator_flag_is_invalid'] = '管理员标记为无效。';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = '硬配额是无效的。';
+$lang['kopano_extension_quota_override'] = '配额覆盖';
+$lang['kopano_extension_soft_quota'] = '软件报价';
+$lang['kopano_extension_warning_quota'] = '警告配额';
