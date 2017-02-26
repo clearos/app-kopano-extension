af5ac4bc8e14bd3f0dcbd5c8bc46908a7629bb15
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'unio Pligrandigo';
+$lang['kopano_extension_app_description'] = 'Unio Extension priskribo'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Unio Kontoj Pligrandigo';
+$lang['kopano_extension_administrator_privileges'] = 'administranto Privilegioj';
+$lang['kopano_extension_hard_quota'] = 'hard Kotizo';
+$lang['kopano_extension_account'] = 'konto';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Konto flago estas nevalida.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administranto flago estas nevalida.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvoto estas nevalida.';
+$lang['kopano_extension_quota_override'] = 'kvoto Override';
+$lang['kopano_extension_soft_quota'] = 'programaro Citaĵo';
+$lang['kopano_extension_warning_quota'] = 'Averto Kotizo';
