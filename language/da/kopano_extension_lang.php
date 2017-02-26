c6299ecbc7b3bb0a4bfc2149910f93a45df6bd2f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension beskrivelse'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Konti Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator privilegier';
+$lang['kopano_extension_hard_quota'] = 'Hård Kvote';
+$lang['kopano_extension_account'] = 'Konto';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Konto flag er ugyldig.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator flag er ugyldig.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvote er ugyldig.';
+$lang['kopano_extension_quota_override'] = 'Kvote Tilsidesæt';
+$lang['kopano_extension_soft_quota'] = 'software Quote';
+$lang['kopano_extension_warning_quota'] = 'Advarsel Kvote';
