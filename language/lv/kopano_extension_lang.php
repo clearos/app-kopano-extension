4e8531e8c1764d2e86aa119f6b879006aa7a5e30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension apraksts'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union konti Extension';
+$lang['kopano_extension_administrator_privileges'] = 'administratora privilēģijas';
+$lang['kopano_extension_hard_quota'] = 'Hard kvota';
+$lang['kopano_extension_account'] = 'Konts';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Konts karogs ir nederīgs.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrators karogs ir nederīgs.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvota ir nederīgs.';
+$lang['kopano_extension_quota_override'] = 'kvota Ignorēt';
+$lang['kopano_extension_soft_quota'] = 'programmatūra Citēt';
+$lang['kopano_extension_warning_quota'] = 'Brīdinājums kvota';
