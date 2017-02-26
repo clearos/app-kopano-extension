9a169882d8311e97ce1aca2462cc6a7b9bbfd011
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ផ្នែកបន្ថែមសហជីព';
+$lang['kopano_extension_app_description'] = 'ការរៀបរាប់ផ្នែកបន្ថែមសហជីព'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ផ្នែកបន្ថែមគណនីសហជីព';
+$lang['kopano_extension_administrator_privileges'] = 'សិទ្ធិគ្រប់គ្រង';
+$lang['kopano_extension_hard_quota'] = 'កូតារឹង';
+$lang['kopano_extension_account'] = 'គណនី';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ទង់ជាតិគណនីមិនត្រឹមត្រូវ។';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'ទង់ជាតិអ្នកគ្រប់គ្រងមិនត្រឹមត្រូវ។';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'កូតារឹងមិនត្រឹមត្រូវ។';
+$lang['kopano_extension_quota_override'] = 'បដិសេធកូតា';
+$lang['kopano_extension_soft_quota'] = 'សម្រង់ផ្នែកទន់';
+$lang['kopano_extension_warning_quota'] = 'កូតាការព្រមាន';
