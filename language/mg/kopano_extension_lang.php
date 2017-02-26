a091b0be31330105085923243ea1653cadf7bfc8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension Famaritana'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Kaonty Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator Tombontsoa';
+$lang['kopano_extension_hard_quota'] = 'Hard anjaranareo';
+$lang['kopano_extension_account'] = 'Account';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Account sainam-pirenena dia tsy manan-kery.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator sainam-pirenena dia tsy manan-kery.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard anjaranareo no tsy manan-kery.';
+$lang['kopano_extension_quota_override'] = 'anjaranareo handresy ny';
+$lang['kopano_extension_soft_quota'] = 'rindrambaiko Quote';
+$lang['kopano_extension_warning_quota'] = 'fampitandremana anjaranareo';
