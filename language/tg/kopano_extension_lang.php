404e0af014f794f72222145105b737993618591a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:12:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Иттиҳоди Тамдиди';
+$lang['kopano_extension_app_description'] = 'Шарҳи Иттиҳоди Тамдиди'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Иттиҳоди ҳисоб Тамдиди';
+$lang['kopano_extension_administrator_privileges'] = 'Имтиёзҳои Administrator';
+$lang['kopano_extension_hard_quota'] = 'квотаи Хард';
+$lang['kopano_extension_account'] = 'Ҳисоби';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Парчами Ҳисоби нодуруст аст.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Парчами маъмур нодуруст аст.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'квотаи Hard нодуруст аст.';
+$lang['kopano_extension_quota_override'] = 'ҷойгузор квотаи';
+$lang['kopano_extension_soft_quota'] = 'нармафзори Quote';
+$lang['kopano_extension_warning_quota'] = 'квотаи Огоҳӣ';
