d7e94fb79ccf1e5212980c8a3ad23ad329a2cdea
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:13:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Birlik Uzantısı';
+$lang['kopano_extension_app_description'] = 'Birlik Uzatma açıklaması'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Birlik Hesaplar Uzantısı';
+$lang['kopano_extension_administrator_privileges'] = 'yönetici Ayrıcalıkları';
+$lang['kopano_extension_hard_quota'] = 'sert Kota';
+$lang['kopano_extension_account'] = 'hesap';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Hesap bayrağı geçersiz.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Yönetici bayrağı geçersiz.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Sabit kota geçersiz.';
+$lang['kopano_extension_quota_override'] = 'kota Geçersiz Kıl';
+$lang['kopano_extension_soft_quota'] = 'yazılım Alıntı';
+$lang['kopano_extension_warning_quota'] = 'uyarı Kotası';
