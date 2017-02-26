14f1b500cccb9ad4240f5db4951a97848d61ddda
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:06:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Síneadh Union';
+$lang['kopano_extension_app_description'] = 'Síneadh Union síos'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Extension Cuntais Union';
+$lang['kopano_extension_administrator_privileges'] = 'Pribhléidí riarthóir';
+$lang['kopano_extension_hard_quota'] = 'Cuóta crua';
+$lang['kopano_extension_account'] = 'Cuntas';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Is bratach Cuntas neamhbhailí.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Is bratach Riarthóir neamhbhailí.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Is cuóta crua neamhbhailí.';
+$lang['kopano_extension_quota_override'] = 'Sáraigh cuóta';
+$lang['kopano_extension_soft_quota'] = 'bogearraí Athfhriotail';
+$lang['kopano_extension_warning_quota'] = 'Cuóta rabhadh';
