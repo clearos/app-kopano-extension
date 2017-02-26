cf534b87cd6e0ac1c2502ff51c690d22f7d509c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:05:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Ένωση Επέκταση';
+$lang['kopano_extension_app_description'] = 'Περιγραφή Ένωσης Παράταση'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Ένωση Λογαριασμοί Επέκταση';
+$lang['kopano_extension_administrator_privileges'] = 'Προνόμια διαχειριστή';
+$lang['kopano_extension_hard_quota'] = 'σκληρό Ποσόστωση';
+$lang['kopano_extension_account'] = 'Λογαριασμός';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'σημαία του λογαριασμού δεν είναι έγκυρο.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'σημαία διαχειριστή δεν είναι έγκυρο.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Σκληρό ποσόστωση είναι άκυρη.';
+$lang['kopano_extension_quota_override'] = 'ποσόστωση Παράκαμψη';
+$lang['kopano_extension_soft_quota'] = 'παράθεση λογισμικού';
+$lang['kopano_extension_warning_quota'] = 'προειδοποίηση Ποσόστωση';
