9d4e9daa0594b36d6b3f16c92dbedd1bf14697fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:10:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'union Extension';
+$lang['kopano_extension_app_description'] = 'Descriere union Extensie'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Conturi de extensie din Uniune';
+$lang['kopano_extension_administrator_privileges'] = 'privilegii de administrator';
+$lang['kopano_extension_hard_quota'] = 'greu Cotă';
+$lang['kopano_extension_account'] = 'Cont';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'pavilion cont nu este valid.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administratorul de pavilion este nevalid.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'contingent greu este nevalid.';
+$lang['kopano_extension_quota_override'] = 'cotă Suprascriere';
+$lang['kopano_extension_soft_quota'] = 'software Citat';
+$lang['kopano_extension_warning_quota'] = 'Cotă de avertizare';
