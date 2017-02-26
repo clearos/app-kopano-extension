8c381cbe5c15cb32468abcf775b39189ed8512bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Unione extensio';
+$lang['kopano_extension_app_description'] = 'Extensionem iuris Description'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Ratio extensionem iuris';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator privilegiorum';
+$lang['kopano_extension_hard_quota'] = 'ferreus-orig.doc';
+$lang['kopano_extension_account'] = 'account';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Vexillum causa nulla est.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator vexillum est invalidum.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Iuxta numerum, invalidum est.';
+$lang['kopano_extension_quota_override'] = 'quota Override';
+$lang['kopano_extension_soft_quota'] = 'Quote software';
+$lang['kopano_extension_warning_quota'] = 'Quota Warning';
