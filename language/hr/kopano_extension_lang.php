342fd9014ec33b64225c14627ae66fe9364240c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'unija za proširenje';
+$lang['kopano_extension_app_description'] = 'Opis unije za proširenje'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Unija računi Produžni';
+$lang['kopano_extension_administrator_privileges'] = 'administratorske ovlasti';
+$lang['kopano_extension_hard_quota'] = 'Tvrdi kvota';
+$lang['kopano_extension_account'] = 'Račun';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Račun zastava je nevažeći.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator zastava je nevažeći.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Tvrdi kvota je nevažeći.';
+$lang['kopano_extension_quota_override'] = 'Kvota Poništi';
+$lang['kopano_extension_soft_quota'] = 'softver Citat';
+$lang['kopano_extension_warning_quota'] = 'Kvota Upozorenje';
