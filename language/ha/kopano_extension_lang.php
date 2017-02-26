ac35ad2a7b6b3c613816a086c972f3c4abfd0f66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:06:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Tsawo';
+$lang['kopano_extension_app_description'] = 'Union Tsawo description'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Accounts Tsawo';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator gata';
+$lang['kopano_extension_hard_quota'] = 'Hard Ƙa&#39;idar';
+$lang['kopano_extension_account'] = 'account';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Account flag ba daidai ba ne.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator flag ba daidai ba ne.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard keɓaɓɓen yawa ba daidai ba ne.';
+$lang['kopano_extension_quota_override'] = 'Ƙa&#39;idar override';
+$lang['kopano_extension_soft_quota'] = 'software Quote';
+$lang['kopano_extension_warning_quota'] = 'gargadi Ƙa&#39;idar';
