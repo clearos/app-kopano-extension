efaf99da2451e810276b1d7fff4a044ca78a807d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:06:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension lýsing'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union reikningar Extension';
+$lang['kopano_extension_administrator_privileges'] = 'stjórnandi Forréttindi';
+$lang['kopano_extension_hard_quota'] = 'Hard Kvóti';
+$lang['kopano_extension_account'] = 'Reikningur';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Reikningur fáninn er ógilt.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Stjórnandi fáninn er ógilt.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvóta er ógilt.';
+$lang['kopano_extension_quota_override'] = 'kvóti Hunsa';
+$lang['kopano_extension_soft_quota'] = 'hugbúnaður Quote';
+$lang['kopano_extension_warning_quota'] = 'Viðvörun Kvóti';
