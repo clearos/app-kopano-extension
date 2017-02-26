685e9562696e92c550caf0c70632dbea61f05729
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'კავშირის გაფართოება';
+$lang['kopano_extension_app_description'] = 'კავშირის გაფართოება აღწერა'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'კავშირის ანგარიშების გაფართოება';
+$lang['kopano_extension_administrator_privileges'] = 'ადმინისტრატორის პრივილეგიები';
+$lang['kopano_extension_hard_quota'] = 'Hard კვოტა';
+$lang['kopano_extension_account'] = 'ანგარიშის';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ანგარიშის დროშა არასწორია.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator დროშა არასწორია.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard კვოტა არასწორია.';
+$lang['kopano_extension_quota_override'] = 'კვოტების გადააჭარბე';
+$lang['kopano_extension_soft_quota'] = 'პროგრამული Quote';
+$lang['kopano_extension_warning_quota'] = 'გაფრთხილება კვოტა';
