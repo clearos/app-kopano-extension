d93d792e03e46770cc5e52b2f1ad61c2c4afd404
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:03:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ইউনিয়ন সম্প্রসারণ';
+$lang['kopano_extension_app_description'] = 'ইউনিয়ন সম্প্রসারণ বিবরণ'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ইউনিয়ন অ্যাকাউন্টস এক্সটেনশন';
+$lang['kopano_extension_administrator_privileges'] = 'অ্যাডমিনিস্ট্রেটর প্রিভিলেজেস';
+$lang['kopano_extension_hard_quota'] = 'হার্ড কোটা';
+$lang['kopano_extension_account'] = 'হিসাব';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'অ্যাকাউন্ট পতাকা অবৈধ.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'অ্যাডমিনিস্ট্রেটর পতাকা অবৈধ.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'হার্ড কোটা অবৈধ.';
+$lang['kopano_extension_quota_override'] = 'কোটা ওভাররাইড';
+$lang['kopano_extension_soft_quota'] = 'সফ্টওয়্যার উক্তি';
+$lang['kopano_extension_warning_quota'] = 'সতর্কতা কোটা';
