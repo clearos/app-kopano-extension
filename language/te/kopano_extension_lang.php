f03e5a11f50cb1a52597e832d064fbd4292f0710
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:12:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'యూనియన్ పొడిగింపు';
+$lang['kopano_extension_app_description'] = 'యూనియన్ పొడిగింపు వివరణ'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'కేంద్ర అకౌంట్స్ పొడిగింపు';
+$lang['kopano_extension_administrator_privileges'] = 'నిర్వాహక అధికారాలను';
+$lang['kopano_extension_hard_quota'] = 'హార్డ్ కోటా';
+$lang['kopano_extension_account'] = 'ఖాతా';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ఖాతా జెండా చెల్లదు.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'వ్యవస్థ పాలకుని జెండా చెల్లదు.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'హార్డ్ కోటా చెల్లదు.';
+$lang['kopano_extension_quota_override'] = 'కోటా భర్తీ';
+$lang['kopano_extension_soft_quota'] = 'సాఫ్ట్వేర్ కోట్';
+$lang['kopano_extension_warning_quota'] = 'హెచ్చరిక కోటా';
