558da1cdb97a72ad88ac306df41513636791ead3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:14:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'incazelo Union Extension'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator Amalungelo';
+$lang['kopano_extension_hard_quota'] = 'Quota Hard';
+$lang['kopano_extension_account'] = '-Akhawunti';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'flag-Akhawunti ayivumelekile.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator flag ayivumelekile.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'quota Hard ayivumelekile.';
+$lang['kopano_extension_quota_override'] = 'Quota Dlova';
+$lang['kopano_extension_soft_quota'] = 'isofthiwe Quote';
+$lang['kopano_extension_warning_quota'] = 'Isexwayiso Quota';
