e03878dfae9f4bc7a51377d2f921d0b5ea0b5ac1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Rozšíření';
+$lang['kopano_extension_app_description'] = 'Popis Union Rozšíření'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union účty Extension';
+$lang['kopano_extension_administrator_privileges'] = 'administrátorskými právy';
+$lang['kopano_extension_hard_quota'] = 'Hard Kvóta';
+$lang['kopano_extension_account'] = 'Účet';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'vlajka účtu je neplatný.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'vlajka správce je neplatný.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvóta je neplatný.';
+$lang['kopano_extension_quota_override'] = 'kvóta Přepis';
+$lang['kopano_extension_soft_quota'] = 'software Citace';
+$lang['kopano_extension_warning_quota'] = 'Varování Kvóta';
