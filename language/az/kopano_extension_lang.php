8f0d120ae2d575386c8d88857bef9152fc67ad88
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:02:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension təsviri'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Administrator güzəştlər';
+$lang['kopano_extension_hard_quota'] = 'Hard Quota';
+$lang['kopano_extension_account'] = 'hesab';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Hesab bayraq yanlışdır.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator bayraq yanlışdır.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvota yanlışdır.';
+$lang['kopano_extension_quota_override'] = 'Quota yalnış';
+$lang['kopano_extension_soft_quota'] = 'proqram Quote';
+$lang['kopano_extension_warning_quota'] = 'Warning Quota';
