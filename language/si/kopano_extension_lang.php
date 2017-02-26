8c5767832f87eeb7f0efed5680f0b33d66ba0966
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:11:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'වෘත්තීය සමිති ව්යාප්ති';
+$lang['kopano_extension_app_description'] = 'වෘත්තීය සමිති ව්යාප්ති විස්තරයක්'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'වෘත්තීය සමිති ගිණුම් ව්යාප්ති';
+$lang['kopano_extension_administrator_privileges'] = 'පරිපාලක වරප්රසාද';
+$lang['kopano_extension_hard_quota'] = 'Hard පංගු';
+$lang['kopano_extension_account'] = 'ගිණුම';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ගිණුම ධජය වලංගු නැත.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'පරිපාලක ධජය වලංගු නැත.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard කෝටා වලංගු නැත.';
+$lang['kopano_extension_quota_override'] = 'කෝටා ජීවය';
+$lang['kopano_extension_soft_quota'] = 'මෘදුකාංග Quote';
+$lang['kopano_extension_warning_quota'] = 'අවවාදයයි පංගු';
