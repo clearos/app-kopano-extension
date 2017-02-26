850ac23a37ebc31c81094c2de210342f81de916a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Liidu pikendamine';
+$lang['kopano_extension_app_description'] = 'Liidu Extension kirjeldus'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Liidu kontod pikendamine';
+$lang['kopano_extension_administrator_privileges'] = 'administraatori õigusi';
+$lang['kopano_extension_hard_quota'] = 'karm piirang';
+$lang['kopano_extension_account'] = 'Konto';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Konto lipp on kehtetu.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administraator lipp on kehtetu.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard kvoot on kehtetu.';
+$lang['kopano_extension_quota_override'] = 'kvoot alistamine';
+$lang['kopano_extension_soft_quota'] = 'tarkvara Quote';
+$lang['kopano_extension_warning_quota'] = 'Hoiatus kvoot';
