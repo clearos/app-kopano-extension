4d22100da1afbc858ee957d80bb9da65fb38ce66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:02:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension նկարագրությունը'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union հաշիվներ Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Ադմինիստրատոր Արտոնություններ';
+$lang['kopano_extension_hard_quota'] = 'Hard Սահմանը';
+$lang['kopano_extension_account'] = 'հաշիվ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Հաշվի դրոշը անվավեր է:';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator դրոշը անվավեր է:';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard քվոտան է անվավեր:';
+$lang['kopano_extension_quota_override'] = 'քվոտա Վերագրել';
+$lang['kopano_extension_soft_quota'] = 'ծրագրային Մեջբերում';
+$lang['kopano_extension_warning_quota'] = 'Զգուշացում Սահմանը';
