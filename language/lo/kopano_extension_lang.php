31fa44effda7ec9695294c610ec151b4cce06a19
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ການຂະຫຍາຍສະຫະພາບ';
+$lang['kopano_extension_app_description'] = 'ລາຍລະອຽດຂອງສະຫະພາບການຂະຫຍາຍ'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ສະຫະພາບແຮງບັນຊີການຂະຫຍາຍ';
+$lang['kopano_extension_administrator_privileges'] = 'ສິດທິພິເສດ Administrator';
+$lang['kopano_extension_hard_quota'] = 'Quota ຍາກ';
+$lang['kopano_extension_account'] = 'ບັນຊີ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ທຸງຊາດບັນຊີບໍ່ຖືກຕ້ອງ.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'ທຸງຊາດບໍລິຫານແມ່ນບໍ່ຖືກຕ້ອງ.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ໂຄຕາການຍາກທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['kopano_extension_quota_override'] = 'ແທນໂກຕາ';
+$lang['kopano_extension_soft_quota'] = 'ອ້າງຊອບແວ';
+$lang['kopano_extension_warning_quota'] = 'Quota ການເຕືອນໄພ';
