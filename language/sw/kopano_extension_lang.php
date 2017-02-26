e66f4218ae0d8f8262154f56a9d6165f74547b20
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:12:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union ugani';
+$lang['kopano_extension_app_description'] = 'Union ugani maelezo'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Hesabu za ugani';
+$lang['kopano_extension_administrator_privileges'] = 'msimamizi Haki';
+$lang['kopano_extension_hard_quota'] = 'Hard Viti Maalum';
+$lang['kopano_extension_account'] = 'akaunti';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Akaunti bendera ni batili.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Msimamizi bendera ni batili.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard viti maalum ni batili.';
+$lang['kopano_extension_quota_override'] = 'Viti Maalum override';
+$lang['kopano_extension_soft_quota'] = 'programu Quote';
+$lang['kopano_extension_warning_quota'] = 'onyo Viti Maalum';
