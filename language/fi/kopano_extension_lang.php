42a00ec2748c06160d7aedebb43e3f9bda8a2ed3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:05:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension kuvaus'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union tilit Extension';
+$lang['kopano_extension_administrator_privileges'] = 'järjestelmänvalvojan oikeudet';
+$lang['kopano_extension_hard_quota'] = 'Hard kiintiö';
+$lang['kopano_extension_account'] = 'Tili';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Tilin lippu ei kelpaa.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Ylläpitäjä lippu ei kelpaa.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Kova kiintiö on virheellinen.';
+$lang['kopano_extension_quota_override'] = 'kiintiö ohitus';
+$lang['kopano_extension_soft_quota'] = 'ohjelmisto Quote';
+$lang['kopano_extension_warning_quota'] = 'Varoitus kiintiö';
