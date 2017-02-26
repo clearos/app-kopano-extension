84799a7a9adc8b5a75af173b8384bfa65e48f4cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:13:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'یونین کی توسیع';
+$lang['kopano_extension_app_description'] = 'یونین کی توسیع تفصیل'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'یونین اکاؤنٹس کی توسیع';
+$lang['kopano_extension_administrator_privileges'] = 'ایڈمنسٹریٹر کی سہولیات';
+$lang['kopano_extension_hard_quota'] = 'ہارڈ کوٹہ';
+$lang['kopano_extension_account'] = 'اکاؤنٹ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'اکاؤنٹ پرچم غلط ہے.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'ایڈمنسٹریٹر پرچم غلط ہے.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ہارڈ کوٹہ غلط ہے.';
+$lang['kopano_extension_quota_override'] = 'کوٹہ کی جگہ لے لے';
+$lang['kopano_extension_soft_quota'] = 'سافٹ ویئر کے اقتباس';
+$lang['kopano_extension_warning_quota'] = 'انتباہ کا کوٹا';
