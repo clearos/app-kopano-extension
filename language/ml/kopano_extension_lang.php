88d639c9074cbe93ecd76b08596e4c8d8bffe078
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'യൂണിയൻ വിപുലീകരണം';
+$lang['kopano_extension_app_description'] = 'യൂണിയൻ വിപുലീകരണ വിവരണം'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'കേന്ദ്ര അക്കൗണ്ടുകൾ വിപുലീകരണം';
+$lang['kopano_extension_administrator_privileges'] = 'രക്ഷാധികാരി അധികാരങ്ങൾ';
+$lang['kopano_extension_hard_quota'] = 'ഹാർഡ് ക്വാട്ട';
+$lang['kopano_extension_account'] = 'കണക്ക്';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'അക്കൗണ്ട് പതാക അസാധുവാണ്.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'അഡ്മിനിസ്ട്രേറ്റർ പതാക അസാധുവാണ്.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ഹാർഡ് ക്വാട്ട അസാധുവാണ്.';
+$lang['kopano_extension_quota_override'] = 'ക്വാട്ട ഓവർറൈട്';
+$lang['kopano_extension_soft_quota'] = 'സോഫ്റ്റ്വെയർ ഉദ്ധരിക്കുക';
+$lang['kopano_extension_warning_quota'] = 'മുന്നറിയിപ്പ് ക്വാട്ട';
