c721d60f00f1653271afe5779773953b050093b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:09:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Unjoni Estensjoni';
+$lang['kopano_extension_app_description'] = 'Deskrizzjoni Unjoni Estensjoni'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Unjoni Kontijiet Estensjoni';
+$lang['kopano_extension_administrator_privileges'] = 'Privileġġi amministratur';
+$lang['kopano_extension_hard_quota'] = 'kwota iebsa';
+$lang['kopano_extension_account'] = 'kont';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'bandiera kont huwa invalidu.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'bandiera Amministratur huwa invalidu.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'kwota iebsa hija invalida.';
+$lang['kopano_extension_quota_override'] = 'Jwarrab kwota';
+$lang['kopano_extension_soft_quota'] = 'Quote softwer';
+$lang['kopano_extension_warning_quota'] = 'kwota twissija';
