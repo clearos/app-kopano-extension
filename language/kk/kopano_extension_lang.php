69b63ca0b6ab867db98c47f0d59e64a141adfcd5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'одақ ұзарту';
+$lang['kopano_extension_app_description'] = 'Одақ Кеңейтім сипаттамасы'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Одақ Есеп ұзарту';
+$lang['kopano_extension_administrator_privileges'] = 'әкімші құқықтары';
+$lang['kopano_extension_hard_quota'] = 'қатты квота';
+$lang['kopano_extension_account'] = 'шот';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Шот туы жарамсыз болып табылады.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Әкімші туы жарамсыз болып табылады.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Қатты квота жарамсыз болып табылады.';
+$lang['kopano_extension_quota_override'] = 'квота Override';
+$lang['kopano_extension_soft_quota'] = 'бағдарламалық қамтамасыз ету дәйексөз';
+$lang['kopano_extension_warning_quota'] = 'Ескерту квота';
