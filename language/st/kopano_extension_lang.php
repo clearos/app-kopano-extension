c7deff624f6b4e543cda514d527bbf6ff3541a9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:11:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Kopano Extension';
+$lang['kopano_extension_app_description'] = 'Kopano Extension description'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Kopano Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'mohlokomeli oa Litokelo';
+$lang['kopano_extension_hard_quota'] = 'Hard palo';
+$lang['kopano_extension_account'] = 'Account';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Account folakha ha o sebetse.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Mohlokomeli oa folakha ha o sebetse.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard palo ha o sebetse.';
+$lang['kopano_extension_quota_override'] = 'palo hatelle';
+$lang['kopano_extension_soft_quota'] = 'Software Quote';
+$lang['kopano_extension_warning_quota'] = 'temoso ea palo';
