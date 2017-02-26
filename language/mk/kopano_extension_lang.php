094c82539a596fb0e210963f778045065c82735a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Продолжување унија';
+$lang['kopano_extension_app_description'] = 'Опис унија Продолжување'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Унија сметки Продолжување';
+$lang['kopano_extension_administrator_privileges'] = 'администраторски привилегии';
+$lang['kopano_extension_hard_quota'] = 'хард квоти';
+$lang['kopano_extension_account'] = 'сметка';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'знаме сметка е неважечка.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Администратор знаме не е валиден.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Хард квотата не е валиден.';
+$lang['kopano_extension_quota_override'] = 'квота Отфрли';
+$lang['kopano_extension_soft_quota'] = 'софтвер Цитат';
+$lang['kopano_extension_warning_quota'] = 'Предупредување квоти';
