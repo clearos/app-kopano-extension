afe308d15c4171224659830142d25b94f74e6c81
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ಯೂನಿಯನ್ ವಿಸ್ತರಣೆ';
+$lang['kopano_extension_app_description'] = 'ಯೂನಿಯನ್ ವಿಸ್ತರಣೆ ವಿವರಣೆ'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ಯೂನಿಯನ್ ಖಾತೆಗಳು ವಿಸ್ತರಣೆ';
+$lang['kopano_extension_administrator_privileges'] = 'ನಿರ್ವಾಹಕ ಸೌಲಭ್ಯಗಳನ್ನು';
+$lang['kopano_extension_hard_quota'] = 'ಹಾರ್ಡ್ ಕೋಟಾ';
+$lang['kopano_extension_account'] = 'ಖಾತೆ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ಖಾತೆ ಧ್ವಜ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'ನಿರ್ವಾಹಕ ಧ್ವಜ ಅಮಾನ್ಯವಾಗಿದೆ.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ಹಾರ್ಡ್ ಕೋಟಾ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['kopano_extension_quota_override'] = 'ಕೋಟಾ ಅತಿಕ್ರಮಿಸಲಾಗಿದೆ';
+$lang['kopano_extension_soft_quota'] = 'ತಂತ್ರಾಂಶ ಉದ್ಧರಣ';
+$lang['kopano_extension_warning_quota'] = 'ಎಚ್ಚರಿಕೆ ಕೋಟಾ';
