e5e3f88c9b516565cab14d7022152c7f4567a9ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:10:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Союз Extension';
+$lang['kopano_extension_app_description'] = 'Описание Extension Union'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Союз Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'привилегии администратора';
+$lang['kopano_extension_hard_quota'] = 'Квота';
+$lang['kopano_extension_account'] = 'Счет';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Флаг счета недействительно.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'флаг администратора является недействительным.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Квота является недействительным.';
+$lang['kopano_extension_quota_override'] = 'Квота Override';
+$lang['kopano_extension_soft_quota'] = 'программное обеспечение Цитировать';
+$lang['kopano_extension_warning_quota'] = 'Внимание Квота';
