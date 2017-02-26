4f22875acd2515e9c135dad02391a6c2e47bbc66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:03:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'саюз Extension';
+$lang['kopano_extension_app_description'] = 'Апісанне Extension Union'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Саюз Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'прывілеі адміністратара';
+$lang['kopano_extension_hard_quota'] = 'квота';
+$lang['kopano_extension_account'] = 'рахункі';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Сцяг рахункі несапраўднае.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'сцяг адміністратара з&#39;яўляецца несапраўдным.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Квота з&#39;яўляецца несапраўдным.';
+$lang['kopano_extension_quota_override'] = 'квота Override';
+$lang['kopano_extension_soft_quota'] = 'праграмнае забеспячэнне Цытаваць';
+$lang['kopano_extension_warning_quota'] = 'Увага Квота';
