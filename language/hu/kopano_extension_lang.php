5d12ef899df107eac7155e04e34069ca44242262
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:06:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Unió Extension';
+$lang['kopano_extension_app_description'] = 'Unió Extension leírás'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Unió számlák kiterjesztése';
+$lang['kopano_extension_administrator_privileges'] = 'rendszergazdai jogosultságok';
+$lang['kopano_extension_hard_quota'] = 'kemény kvóta';
+$lang['kopano_extension_account'] = 'Fiók';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Fiók zászló érvénytelen.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator zászló érvénytelen.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Kemény kvóta érvénytelen.';
+$lang['kopano_extension_quota_override'] = 'kvóta felülírása';
+$lang['kopano_extension_soft_quota'] = 'szoftver Idézet';
+$lang['kopano_extension_warning_quota'] = 'Figyelmeztetés kvóta';
