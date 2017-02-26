285ae15498922abc7b8bc8039ced5b4327442337
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:08:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Toronga';
+$lang['kopano_extension_app_description'] = 'Whakaahuatanga Union Toronga'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Accounts Toronga';
+$lang['kopano_extension_administrator_privileges'] = 'Painga kaiwhakahaere';
+$lang['kopano_extension_hard_quota'] = 'Quota Hard';
+$lang['kopano_extension_account'] = 'pūkete';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'He muhu haki Pūkete.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'He muhu Kaiwhakahaere kara.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'He muhu roherohenga Hard.';
+$lang['kopano_extension_quota_override'] = 'Whakaawe Quota';
+$lang['kopano_extension_soft_quota'] = 'Quote pūmanawa';
+$lang['kopano_extension_warning_quota'] = 'Quota whakatūpato';
