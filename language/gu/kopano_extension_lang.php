4a1be856a00cd0fbb534563ebce6ca787bf4cad7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:05:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'યુનિયન એક્સ્ટેંશન';
+$lang['kopano_extension_app_description'] = 'યુનિયન એક્સ્ટેન્શન વર્ણન'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'યુનિયન એકાઉન્ટ્સ એક્સ્ટેંશન';
+$lang['kopano_extension_administrator_privileges'] = 'સંચાલક વિશેષાધિકારો';
+$lang['kopano_extension_hard_quota'] = 'હાર્ડ ક્વોટા';
+$lang['kopano_extension_account'] = 'એકાઉન્ટ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'એકાઉન્ટ ધ્વજ અમાન્ય છે.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'સંચાલક ધ્વજ અમાન્ય છે.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'હાર્ડ ક્વોટા અમાન્ય છે.';
+$lang['kopano_extension_quota_override'] = 'ક્વોટા પર ફરીથી લખો';
+$lang['kopano_extension_soft_quota'] = 'સોફ્ટવેર ભાવ';
+$lang['kopano_extension_warning_quota'] = 'ચેતવણી ક્વોટા';
