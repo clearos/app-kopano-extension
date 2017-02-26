6081aa629727cc401172cf6b266e71e292e3801c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ユニオン拡張';
+$lang['kopano_extension_app_description'] = 'ユニオン拡張の説明'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ユニオンアカウント拡張';
+$lang['kopano_extension_administrator_privileges'] = '管理者権限';
+$lang['kopano_extension_hard_quota'] = 'ハードクォータ';
+$lang['kopano_extension_account'] = 'アカウント';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'アカウントフラグが無効です。';
+$lang['kopano_extension_administrator_flag_is_invalid'] = '管理者のフラグが無効です。';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ハードクォータは無効です。';
+$lang['kopano_extension_quota_override'] = 'クォータのオーバーライド';
+$lang['kopano_extension_soft_quota'] = 'ソフトウェアの引用';
+$lang['kopano_extension_warning_quota'] = '警告クォータ';
