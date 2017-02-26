330171b8f72e108bdc7560b51b0b891835fa198a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:11:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Midowga Extension';
+$lang['kopano_extension_app_description'] = 'Midowga Extension description'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Midowga Accounts Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Mudnaanta Administrator';
+$lang['kopano_extension_hard_quota'] = 'kootada Hard';
+$lang['kopano_extension_account'] = 'Account';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'calanka Account waa baadil.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'calanka Maamulaha waa baadil.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'kootada adag waa baadil.';
+$lang['kopano_extension_quota_override'] = 'tirtirto kootada';
+$lang['kopano_extension_soft_quota'] = 'Quote software';
+$lang['kopano_extension_warning_quota'] = 'kootada Digniin';
