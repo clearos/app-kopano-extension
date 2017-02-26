553829d0d9a4c3068b07c858139a68fb19cafff6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:02:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension azalpena'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Kontuak Extension';
+$lang['kopano_extension_administrator_privileges'] = 'administratzaile eskubideak';
+$lang['kopano_extension_hard_quota'] = 'Hard Kupoari';
+$lang['kopano_extension_account'] = 'kontua';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Kontua ez da baliozkoa.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator bandera baliogabea da.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Hard-kuota baliogabea da.';
+$lang['kopano_extension_quota_override'] = 'Kupoari Override';
+$lang['kopano_extension_soft_quota'] = 'software Aipamena';
+$lang['kopano_extension_warning_quota'] = 'Abisua Kupoari';
