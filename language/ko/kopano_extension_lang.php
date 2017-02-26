5fc6bccf85c76a706f0e7809db1494a1079f84df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:07:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = '연합 확장';
+$lang['kopano_extension_app_description'] = '연합 (EU) 확장 설명'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = '연합 계정 확장';
+$lang['kopano_extension_administrator_privileges'] = '관리자 권한';
+$lang['kopano_extension_hard_quota'] = '하드 할당량';
+$lang['kopano_extension_account'] = '계정';
+
+$lang['kopano_extension_account_flag_is_invalid'] = '계정 플래그가 잘못되었습니다.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = '관리자 플래그가 잘못되었습니다.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = '하드 할당량이 잘못되었습니다.';
+$lang['kopano_extension_quota_override'] = '할당량 재정의';
+$lang['kopano_extension_soft_quota'] = '소프트웨어 견적';
+$lang['kopano_extension_warning_quota'] = '경고 할당량';
