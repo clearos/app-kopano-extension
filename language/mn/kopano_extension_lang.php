19b967dfbbe9ef4d4d51aec4d3240aef5b60c6dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:09:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'холбоо Өргөтгөл';
+$lang['kopano_extension_app_description'] = 'Холбоо Өргөтгөл тодорхойлолт'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Холбоо данс Өргөтгөл';
+$lang['kopano_extension_administrator_privileges'] = 'Админ эрх';
+$lang['kopano_extension_hard_quota'] = 'Хатуу Ноогдлын';
+$lang['kopano_extension_account'] = 'Дансны';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Дансны туг хүчин төгөлдөр бус байна.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Administrator туг хүчин төгөлдөр бус байна.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Хатуу квот хүчин төгөлдөр бус байна.';
+$lang['kopano_extension_quota_override'] = 'ноогдлын хэрэгсэхгүй болгож';
+$lang['kopano_extension_soft_quota'] = 'програм хангамж Quote';
+$lang['kopano_extension_warning_quota'] = 'Warning Ноогдлын';
