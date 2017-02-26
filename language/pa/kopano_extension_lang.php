fcb6bad644b636cb124b600c0c29f202b60f4036
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:10:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'ਯੂਨੀਅਨ ਐਕਸ਼ਟੇਸ਼ਨ';
+$lang['kopano_extension_app_description'] = 'ਯੂਨੀਅਨ ਐਕਸ਼ਟੇਸ਼ਨ ਦਾ ਵੇਰਵਾ'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'ਯੂਨੀਅਨ ਖਾਤੇ ਐਕਸ਼ਟੇਸ਼ਨ';
+$lang['kopano_extension_administrator_privileges'] = 'ਐਡਮਿਨਸਟੇਟਰ';
+$lang['kopano_extension_hard_quota'] = 'ਹਾਰਡ ਕੋਟਾ';
+$lang['kopano_extension_account'] = 'ਖਾਤਾ';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'ਖਾਤਾ ਝੰਡਾ ਗਲਤ ਹੈ.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'ਪਰਬੰਧਕ ਝੰਡਾ ਗਲਤ ਹੈ.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'ਹਾਰਡ ਕੋਟੇ ਗਲਤ ਹੈ.';
+$lang['kopano_extension_quota_override'] = 'ਕੋਟਾ ਓਵਰਰਾਈਡ';
+$lang['kopano_extension_soft_quota'] = 'ਸਾਫਟਵੇਅਰ Quote';
+$lang['kopano_extension_warning_quota'] = 'ਚੇਤਾਵਨੀ ਕੋਟਾ';
