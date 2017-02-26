a50382b4dd9cc8a6ec7932a330ed11eec40b3658
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:09:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'संघ विस्तार';
+$lang['kopano_extension_app_description'] = 'संघ विस्तार विवरण'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'संघ लेखा विस्तार';
+$lang['kopano_extension_administrator_privileges'] = 'प्रशासक सुअवसर';
+$lang['kopano_extension_hard_quota'] = 'हार्ड कोटा';
+$lang['kopano_extension_account'] = 'खाता';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'खाता झण्डा अमान्य छ।';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'प्रशासक झण्डा अमान्य छ।';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'हार्ड कोटा अमान्य छ।';
+$lang['kopano_extension_quota_override'] = 'कोटा अधिरोहण';
+$lang['kopano_extension_soft_quota'] = 'सफ्टवेयर भाव';
+$lang['kopano_extension_warning_quota'] = 'चेतावनी कोटा';
