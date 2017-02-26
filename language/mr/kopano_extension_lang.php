95ce94064c27d22df43143f916e534541edb9ef2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:09:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'केंद्रीय विस्तार';
+$lang['kopano_extension_app_description'] = 'केंद्रीय विस्तार वर्णन'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'केंद्रीय खाती विस्तार';
+$lang['kopano_extension_administrator_privileges'] = 'प्रशासक विशेषाधिकार';
+$lang['kopano_extension_hard_quota'] = 'हार्ड कोटा';
+$lang['kopano_extension_account'] = 'खाते';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'खाते ध्वज अवैध आहे.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'प्रशासक ध्वज अवैध आहे.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'हार्ड कोटा अवैध आहे.';
+$lang['kopano_extension_quota_override'] = 'कोटा अधिलिखित';
+$lang['kopano_extension_soft_quota'] = 'सॉफ्टवेअर कोट';
+$lang['kopano_extension_warning_quota'] = 'चेतावणी कोटा';
