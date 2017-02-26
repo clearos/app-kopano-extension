235e6af6c45e0cf7efcd880a55a5058405361417
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:02:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'përshkrimi Union Extension'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Llogaritë Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Privilegjet Administrator';
+$lang['kopano_extension_hard_quota'] = 'Quota Hard';
+$lang['kopano_extension_account'] = 'llogari';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'flamuri Llogaria është e pavlefshme.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'flamuri Administrator është e pavlefshme.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Kuota e vështirë është e pavlefshme.';
+$lang['kopano_extension_quota_override'] = 'Quota Override';
+$lang['kopano_extension_soft_quota'] = 'software Quote';
+$lang['kopano_extension_warning_quota'] = 'Quota Warning';
