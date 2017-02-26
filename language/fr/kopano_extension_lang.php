ccb7ed1afb1b03f5b887d3489c3d9357324b14d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:05:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Description Union Extension'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union des comptes Extension';
+$lang['kopano_extension_administrator_privileges'] = 'Les privilèges d&#39;administrateur';
+$lang['kopano_extension_hard_quota'] = 'dur Quota';
+$lang['kopano_extension_account'] = 'Compte';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Compte drapeau est invalide.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'drapeau de l&#39;administrateur est invalide.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Difficile quota est invalide.';
+$lang['kopano_extension_quota_override'] = 'Quota Override';
+$lang['kopano_extension_soft_quota'] = 'logiciel Quote';
+$lang['kopano_extension_warning_quota'] = 'Attention Quota';
