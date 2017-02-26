5a7cdb257997719fec699f48124efe77c8211146
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:10:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Rozszerzenie Unii';
+$lang['kopano_extension_app_description'] = 'Opis Rozszerzenie Unii'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Rozszerzenie Unii Konta';
+$lang['kopano_extension_administrator_privileges'] = 'Uprawnienia administratora';
+$lang['kopano_extension_hard_quota'] = 'twardy Kontyngent';
+$lang['kopano_extension_account'] = 'Konto';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Flaga konta jest nieprawidłowy.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Flaga administratora jest nieprawidłowy.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Twardy kontyngent jest nieprawidłowy.';
+$lang['kopano_extension_quota_override'] = 'kontyngent Zastąp';
+$lang['kopano_extension_soft_quota'] = 'oprogramowanie Cytat';
+$lang['kopano_extension_warning_quota'] = 'Kontyngent Ostrzeżenie';
