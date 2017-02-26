bba738f49b1b631185b50d8ad0823625156186bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:04:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/kopano_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['kopano_extension_app_name'] = 'Union Extension';
+$lang['kopano_extension_app_description'] = 'Union Extension kufotokoza'; // FIXME
+$lang['kopano_extension_kopano_accounts_extension'] = 'Union Nkhani Extension';
+$lang['kopano_extension_administrator_privileges'] = 'woyang&#39;anira Maudindo';
+$lang['kopano_extension_hard_quota'] = 'zovuta kuumba';
+$lang['kopano_extension_account'] = 'nkhani';
+
+$lang['kopano_extension_account_flag_is_invalid'] = 'Nkhani mbendera opanda pake.';
+$lang['kopano_extension_administrator_flag_is_invalid'] = 'Woyang&#39;anira mbendera opanda pake.';
+
+$lang['kopano_extension_hard_quota_is_invalid'] = 'Zovuta kuumba ndi opanda pake.';
+$lang['kopano_extension_quota_override'] = 'kuumba kunyalanyaza';
+$lang['kopano_extension_soft_quota'] = 'software Mawu';
+$lang['kopano_extension_warning_quota'] = 'chenjezo kuumba';
